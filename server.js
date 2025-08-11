86a3c6a92d12a837671faef6f6f88c616769012f
Anoop Thomase <ahoy_sailorboy@yahoo.comm>
2025-08-11 19:40:00 +0530

    Adding Console Log

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const admin = require('./admin/firebase'); // eslint-disable-line no-unused-vars
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+console.log('[init] Admin projectId =', admin.app().options.projectId);
+
 // ---------- Middleware ----------
 app.use(cors());
 app.use(express.json());
